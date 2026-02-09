85cc11980c6e57f164a68509d005d71ce008296d
Khushi-Singh <khushisingh82072@gmail.com>
2026-02-10 04:30:22 +0530

    build files

---
+++ build/CMakeFiles/dep_manager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dep_manager.dir/src/main.cpp.o"
+  "CMakeFiles/dep_manager.dir/src/main.cpp.o.d"
+  "dep_manager"
+  "dep_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dep_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
